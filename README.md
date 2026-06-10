50f240cfc7cfb93cab8ff078e246fb006f6b8ffc
Ivan Roudakov <roudakov.ivan@gmail.com>
2026-06-11 00:17:25 +0300

    code init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Win Theme Shift
+
+Simple app for handling if user changes their theme from light mode to dark and vice versa
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <windows.h>
+#include <iostream>
+
+bool IsLightMode(HKEY hkey) {
+    DWORD value = 1;
+    DWORD size = sizeof(DWORD);
+
+    RegQueryValueExW(
+        hkey,
+        L"AppsUseLightTheme",
+        nullptr,
+        nullptr,
+        reinterpret_cast<LPBYTE>(&value),
+        &size
+    );
+
+    return value == 0;
+}
+
+int main() {
+    HKEY hkey = nullptr;
+
+    LONG lResult = RegOpenKeyExW(
+        HKEY_CURRENT_USER,
+        L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+        0,
+        KEY_NOTIFY | KEY_READ,
+        &hkey
+    );
+
+    if (lResult != ERROR_SUCCESS) {
+        std::cerr<<"Failed to open registry key "<<lResult<<"\n";
+        return 1;
+    }
+
+    std::cout<<"Current theme is:"<<((IsLightMode(hkey)) ? "dark" : "light") <<"\n";
+    return 0;
+}
