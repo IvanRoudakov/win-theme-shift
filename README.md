99fb656debbc9e0f468341c5d0c8ea8328f12719
Ivan Roudakov <roudakov.ivan@gmail.com>
2026-06-11 16:11:07 +0300

    Add theme switching and configuration storing

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ Simple app for handling if user changes their theme from light mode to dark and 
 
 ### Requirements
 
-MSVC (Build tools for Visual Studio or Visual Studio itself)
+MSVC (Build tools for Visual Studio or Visual Studio itself) that supports C++ 17.
 
 ### Executable compilation
 
-Run `cl.exe /EHsc main.cpp /Fe:win-theme-shift.exe /link advapi32.lib`
+Run `cl.exe /EHsc /std:c++17 main.cpp /Fe:win-theme-shift.exe /link advapi32.lib user32.lib`
  
 *Et voila!*
+
+## Usage
+
+Change theme from light to dark (or vice versa) and setup wallpaper. Then when you swith back to this theme linked wallpaper would be set up.
--- main.cpp
+++ main.cpp
@@ -2,12 +2,34 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <fstream>
+#include <optional>
+
+#define CONFIG_FILE_PATH ".\\config.conf"
+
+// Globals
+HANDLE g_shutdownEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
 
 class RegKeyFailureException : public std::runtime_error {
     public:
     RegKeyFailureException(std::string message) : std::runtime_error(message) {}
 };
 
+struct WallpaperPath {
+    std::optional<std::wstring> lightPath = std::nullopt;
+    std::optional<std::wstring> darkPath = std::nullopt;
+};
+
+BOOL WINAPI ConsoleHandler(DWORD signal) {
+    switch (signal) {
+        case CTRL_C_EVENT:
+        case CTRL_BREAK_EVENT:
+        case CTRL_CLOSE_EVENT:
+            SetEvent(g_shutdownEvent);
+            return TRUE;
+    }
+    return FALSE;
+}
 
 bool IsLightMode(HKEY hkey) throw(RegKeyFailureException) {
     DWORD value = 1;
@@ -69,9 +91,46 @@ std::wstring ReadWallpaperPath(HKEY hkey) throw(RegKeyFailureException) {
     return value;
 }
 
+void CloseKeys(HKEY hKeyTheme, HKEY hKeyWallpaper) {
+    RegCloseKey(hKeyTheme);
+    RegCloseKey(hKeyWallpaper);
+}
+
+BOOL ChangeWallpaper(std::wstring path) {
+    return SystemParametersInfoW(
+        SPI_SETDESKWALLPAPER,
+        0,
+        path.data(),
+        SPIF_UPDATEINIFILE | SPIF_SENDCHANGE
+    );
+}
+
 int main() {
+    std::wifstream ifstream(CONFIG_FILE_PATH);
+
+    WallpaperPath wallpaperPath;
+
+    if (ifstream.is_open()) {
+        std::wstring line;
+        while (std::getline(ifstream, line)) {
+            if (line.find(L"Dark=") == 0) {
+                wallpaperPath.darkPath = line.substr(5, line.size());
+            }
+            if (line.find(L"Light=") == 0) {
+                wallpaperPath.lightPath = line.substr(6, line.size());
+            }
+        }
+    }
+
+    ifstream.close();
+
     HKEY hKeyTheme = nullptr;
     HKEY hKeyWallpaper = nullptr;
+
+    if (!SetConsoleCtrlHandler(ConsoleHandler, TRUE)) {
+        std::cerr<<"Unable to set console handler\n";
+        return 1;
+    }
 
     LONG lResult = RegOpenKeyExW(
         HKEY_CURRENT_USER,
@@ -94,33 +153,135 @@ int main() {
         &hKeyWallpaper
     );
 
+    bool isDarkCurrent = false;
+    std::wstring currentWallPaper;
+
     try {
-        std::cout<<"Current theme is: "<<((IsLightMode(hKeyTheme)) ? "dark" : "light") <<"\n";
-        std::wcout<<L"Current wallpaper is: "<<(ReadWallpaperPath(hKeyWallpaper));
+        isDarkCurrent = IsLightMode(hKeyTheme);
+        currentWallPaper = ReadWallpaperPath(hKeyWallpaper);
     } catch (RegKeyFailureException exeption) {
         std::cerr<<exeption.what()<<"\n";
-        //TODO: add processing RegCloseKey
+        CloseKeys(hKeyTheme, hKeyWallpaper);
         return 1;
     }
 
-    // while (true) {
-    //     lResult = RegNotifyChangeKeyValue(
-    //         hKeyTheme,
-    //         FALSE,
-    //         REG_NOTIFY_CHANGE_LAST_SET,
-    //         nullptr,
-    //         FALSE
-    //     );
+    std::cout<<"Current theme is: "<<((isDarkCurrent) ? "dark" : "light") <<"\n";
+    std::wcout<<"Current walpaper is"<<currentWallPaper<<L"\n";
 
-    //     if (lResult != ERROR_SUCCESS) {
-    //         std::cerr<<"Reg notify failed: "<<lResult<<"\n";
-    //         break;
-    //     }
-        
-    //     std::cout<<"Theme changed: "<<((IsLightMode(hKeyTheme)) ? "dark" : "light") <<"\n";
-    // }
+    if (isDarkCurrent) {
+        wallpaperPath.darkPath = currentWallPaper;
+    } else {
+        wallpaperPath.lightPath = currentWallPaper;
+    }
 
-    RegCloseKey(hKeyTheme);
-    RegCloseKey(hKeyWallpaper);
+    HANDLE hEventTheme = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+    HANDLE hEventWallpaper = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+    HANDLE handles[] = {hEventTheme, g_shutdownEvent, hEventWallpaper};
+
+    while (true) {
+        lResult = RegNotifyChangeKeyValue(
+            hKeyTheme,
+            FALSE,
+            REG_NOTIFY_CHANGE_LAST_SET,
+            hEventTheme,
+            TRUE
+        );
+
+        if (lResult != ERROR_SUCCESS) {
+            std::cerr<<"Reg notify failed: "<<lResult<<"\n";
+            break;
+        }
+
+        lResult = RegNotifyChangeKeyValue(
+            hKeyWallpaper,
+            FALSE,
+            REG_NOTIFY_CHANGE_LAST_SET,
+            hEventWallpaper,
+            TRUE
+        );
+
+        if (lResult != ERROR_SUCCESS) {
+            std::cerr<<"Reg notify failed: "<<lResult<<"\n";
+            break;
+        }
+
+        DWORD waitResult = WaitForMultipleObjects(
+            3,
+            handles,
+            FALSE,
+            INFINITE
+        );
+
+        if (WAIT_OBJECT_0 + 1 == waitResult) {
+            std::cout<<"Bye...\n";
+            break;
+        }
+
+        if (WAIT_OBJECT_0 == waitResult) {
+            try {
+                bool isDarkNew = IsLightMode(hKeyTheme);
+                if (isDarkCurrent != isDarkNew) {
+                    isDarkCurrent = isDarkNew;
+                    std::cout<<"Theme changed to: "<<((isDarkCurrent) ? "dark\n" : "light\n");
+                    if(isDarkCurrent && wallpaperPath.darkPath.has_value()) {
+                        if (!ChangeWallpaper(wallpaperPath.darkPath.value())) {
+                            std::cerr<<"Failed to update wallpaper";
+                            break;
+                        }
+                    }
+                    if(!isDarkCurrent && wallpaperPath.lightPath.has_value()) {
+                        if (!ChangeWallpaper(wallpaperPath.lightPath.value())) {
+                            std::cerr<<"Failed to update wallpaper";
+                            break;
+                        }
+                    }
+                }
+            } catch (RegKeyFailureException exception) {
+                std::cerr<<exception.what()<<"\n";
+                break;
+            }
+        }
+
+        if (WAIT_OBJECT_0 + 2 == waitResult) {
+            try {
+                std::wstring newWallpaper = ReadWallpaperPath(hKeyWallpaper);
+                if (newWallpaper != currentWallPaper) {
+                    currentWallPaper = newWallpaper;
+                    std::wcout<<L"Wallpaper changed to:"<<currentWallPaper<<L"\n";
+                    if (isDarkCurrent) {
+                        wallpaperPath.darkPath = currentWallPaper;
+                    } else {
+                        wallpaperPath.lightPath = currentWallPaper;
+                    }
+                }
+            } catch (RegKeyFailureException exception) {
+                std::cerr<<exception.what()<<"\n";
+                break;
+            }
+        }
+    }
+
+    CloseHandle(hEventTheme);
+    CloseHandle(g_shutdownEvent);
+    CloseKeys(hKeyTheme, hKeyWallpaper);
+
+    std::wofstream ofstream(CONFIG_FILE_PATH);
+
+    if (!ofstream.is_open()) {
+        std::cerr<<"Can't write file";
+        return 1;
+    }
+
+    if (wallpaperPath.darkPath.has_value()) {
+        wallpaperPath.darkPath.value().erase(wallpaperPath.darkPath.value().find_first_of(L'\0') + 1);
+        ofstream<<L"Dark="<<wallpaperPath.darkPath.value()<<L"\n";
+    }
+
+    if (wallpaperPath.lightPath.has_value()) {
+        wallpaperPath.lightPath.value().erase(wallpaperPath.lightPath.value().find_first_of(L'\0') + 1);
+        ofstream<<L"Light="<<wallpaperPath.lightPath.value()<<L"\n";
+    }
+
+    ofstream.close();
     return 0;
 }
